bcf5183b95fc78744bd2f97b27a9ae5eeb7d8b05
Sitthiphon Kaesorn <flook.bsk@gmail.com>
2023-02-08 16:21:32 +0700

    i have add swagger for this.

--- src/main/java/com/flukebsk/quartzschedulerdatabase/QuartzSchedulerDatabaseApplication.java
+++ src/main/java/com/flukebsk/quartzschedulerdatabase/QuartzSchedulerDatabaseApplication.java
@@ -2,6 +2,7 @@ package com.flukebsk.quartzschedulerdatabase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class QuartzSchedulerDatabaseApplication {
